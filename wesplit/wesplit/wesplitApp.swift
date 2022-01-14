6ee454e09a15e5a14be2c6889a1ef43782cd6bb4
Alejandro Mcgarvie <mcgarvie.alejandro@gmail.com>
2022-01-13 19:10:13 -0800

    <project>

---
+++ wesplit/wesplit/wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  wesplitApp.swift
+//  wesplit
+//
+//  Created by Alejandro McGarvie on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
